8dbc9486d41f81b863222a7bd1229d1c74133252
yashhasiaj71 <yashhasija71@gmail.com>
2025-07-31 12:23:32 -0500

    Updated Main

--- app/src/main/java/com/example/rtsoftsolutions/MainActivity.java
+++ app/src/main/java/com/example/rtsoftsolutions/MainActivity.java
@@ -13,7 +13,9 @@ import android.view.View;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,20 +33,41 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import android.util.Log; // For logging the value
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    //changes from madhav
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // Your layout
         Fragment AdminHome = new AdminHome(); // your target fragment
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.ContainerView, AdminHome);
-        transaction.addToBackStack(null); // optional, allows user to press back button to return
-        transaction.commit();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        NavController navController = Navigation.findNavController(this , R.id.ContainerView);
+
+        ImageButton homeB = findViewById(R.id.HomeButton);
+        ImageButton studentB = findViewById(R.id.StudentButton);
+        ImageButton reportB = findViewById(R.id.ReportButton);
+
+        homeB.setOnClickListener(v->{
+            navController.navigate(R.id.action_global_AdminHome);
+        });
+
+        studentB.setOnClickListener(v->{
+            navController.navigate(R.id.action_global_studentmanager);
+        });
+
+
+//      ========= :TODO report fragment button ========
+//       reportB.setOnClickListener(v->{
+//           navController.navigate(R.id.action_global_AdminHome);
+//       });
     }
 }
