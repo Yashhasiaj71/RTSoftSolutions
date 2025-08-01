1e233c4a862a3903672b56f9c8df35c4c4da7eea
yashhasiaj71 <yashhasija71@gmail.com>
2025-07-31 19:06:30 -0500

    save branch1

--- app/src/main/java/com/example/rtsoftsolutions/MainActivity.java
+++ app/src/main/java/com/example/rtsoftsolutions/MainActivity.java
@@ -1,15 +1,10 @@
 package com.example.rtsoftsolutions;
-
 import android.os.Bundle;
-
 import com.google.android.material.snackbar.Snackbar;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
@@ -20,13 +15,9 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.rtsoftsolutions.databinding.ActivityMainBinding;
-
 import android.view.Menu;
 import android.view.MenuItem;
-// In your Activity or Fragment (e.g., in onCreate or onStart)
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -73,4 +64,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
 }
