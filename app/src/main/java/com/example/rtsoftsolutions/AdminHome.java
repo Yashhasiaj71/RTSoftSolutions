c89fc38bb9cdb02d51d49dbe33f86be05984bb66
Madhav112355678 <114206008+Madhav112355678@users.noreply.github.com>
2025-08-01 14:42:30 +0530

    chagnes after merging with branch1

--- app/src/main/java/com/example/rtsoftsolutions/AdminHome.java
+++ app/src/main/java/com/example/rtsoftsolutions/AdminHome.java
@@ -47,14 +47,6 @@ public class AdminHome extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BlankFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static AdminHome newInstance(String param1, String param2) {
         AdminHome fragment = new AdminHome();
