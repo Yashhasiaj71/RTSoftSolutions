5c428375f394371bb30a68e0aef1a8965a3c83c6
Madhav112355678 <114206008+Madhav112355678@users.noreply.github.com>
2025-08-19 19:54:05 +0530

    just a short commit : Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/example/rtsoftsolutions/Fragments/FragmentReport.java
+++ app/src/main/java/com/example/rtsoftsolutions/Fragments/FragmentReport.java
@@ -1,12 +1,18 @@
 package com.example.rtsoftsolutions.Fragments;
 
+import static android.view.View.LAYOUT_DIRECTION_LTR;
+import static android.view.View.LAYOUT_DIRECTION_RTL;
 import static androidx.navigation.fragment.FragmentKt.findNavController;
 
+import android.animation.ObjectAnimator;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -25,7 +31,7 @@ public class FragmentReport extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.report_fragment , container , false) ;
+        return inflater.inflate(R.layout.report_fragment, container, false);
     }
 
     @Override
@@ -44,31 +50,94 @@ public class FragmentReport extends Fragment {
         buttonordinaryreport.setTextOff("");
         state[0] = true;
 
-        if(!buttonordinaryreport.isChecked() && !buttonfeesreport.isChecked()) {
+        if (!buttonordinaryreport.isChecked() && !buttonfeesreport.isChecked()) {
             buttonordinaryreport.setChecked(true);
             replaceInner(new FragmentOrdinaryReport());
             buttonfeesreport.setChecked(false);
         }
 
+        //on checked listeners
+
         buttonordinaryreport.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                state[0] = true;
-                buttonfeesreport.setChecked(false);
-                replaceInner(new FragmentOrdinaryReport());
+            if (state[0]) {
+                buttonordinaryreport.setChecked(true);
+            } else {
+                if (isChecked) {
+                    state[0] = true;
+                    Fragment fragmentB = new FragmentOrdinaryReport();
+                    requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentB).commit();
+                    buttonfeesreport.setChecked(false);
+                    buttonordinaryreport.setChecked(true);
+                    blockanimator(getContext(), arr);
+                }
             }
         });
 
+        // fees report setoncheckedchangelistener
         buttonfeesreport.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            if (isChecked) {
-                state[0] = false;
-                buttonordinaryreport.setChecked(false);
-                replaceInner(new Fragment_feesReport());
+            if (!state[0]) {
+                buttonfeesreport.setChecked(true);
+            } else {
+                if (isChecked) {
+                    state[0] = false;
+                    Fragment fragmentB = new Fragment_feesReport();
+                    requireActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView, fragmentB).commit();
+                    buttonordinaryreport.setChecked(false);
+                    buttonfeesreport.setChecked(true);
+                    Animation Rightmovement = AnimationUtils.loadAnimation(getContext(), R.anim.rightswipe);
+                    Animation fadein = AnimationUtils.loadAnimation(getContext(), R.anim.fadein);
+                    blockanimator(getContext(), arr);
+
+
+                }
             }
         });
+
     }
 
     private void replaceInner(Fragment fragment) {
         FragmentTransaction tx = getChildFragmentManager().beginTransaction();
         tx.replace(R.id.fragmentContainerView, fragment).commit();
+    }
+
+
+    private void animateBlockOnUIThreadrtl(TextView block) {
+        getActivity().runOnUiThread(() -> {
+            ObjectAnimator animator = ObjectAnimator.ofFloat(block, "translationX", 0f, -400f);
+            animator.setDuration(700);
+            animator.start();
+            block.setLayoutDirection(LAYOUT_DIRECTION_LTR);
+        });
+    }
+
+    private void animateBlockOnUIThreadltr(TextView block) {
+        getActivity().runOnUiThread(() -> {
+            ObjectAnimator animator = ObjectAnimator.ofFloat(block, "translationX", 35f);
+            animator.setDuration(700);
+            animator.start();
+            block.setLayoutDirection(LAYOUT_DIRECTION_RTL);
+        });
+    }
+
+
+    private void blockanimator(Context context, TextView[] arr) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < arr.length; i++) {
+                    if (state[0]) {
+                        animateBlockOnUIThreadltr(arr[i]);
+                    } else {
+                        animateBlockOnUIThreadrtl(arr[(arr.length - 1) - i]);
+                    }
+                    try {
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).start();
+
     }
 }
